8bdb866ed08b9ab2213840c79b8b924aab8542e8
ckc <mail@ckc.im>
2015-09-27 13:09:54 +0800

    recover

--- src/DisplayGUISwingImp.java
+++ src/DisplayGUISwingImp.java
@@ -1,7 +1,23 @@
+import java.awt.BorderLayout;
+import javax.swing.*;
+
 public class DisplayGUISwingImp implements DisplayUI{
     
-    
-    public void displayTime(){
+    private JFrame frame;
+    private JLabel label;
+
+    public DisplayGUISwingImp() {
+        frame = new JFrame();
+    	label = new JLabel();
+    	frame.getContentPane().add(label,BorderLayout.CENTER);
+    	frame.setSize(50,50);
+    	frame.show();
+    }
+
+    public void displayTime(String time){
+
+       label.setText(time);
+       label.repaint();
         
     }
 }
